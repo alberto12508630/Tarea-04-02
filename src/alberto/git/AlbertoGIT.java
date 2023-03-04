c53aec96a8887cf5dbae032d4dbcbb6dbb2016b0
nebra <nebra@LAPTOP-5DSVCS25>
2023-03-04 03:58:25 +0100

    Primer Commit AlbertoGIT

---
+++ src/alberto/git/AlbertoGIT.java
@@ -0,0 +1,50 @@
+
+package alberto.git;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alberto López García
+ */
+public class AlbertoGIT {
+
+    public static void main(String[] args) {
+        
+         int dato;
+        int intentos = 10;
+        boolean haGanado = false;
+
+        int numero = (int) ((Math.random() * 61) + 20);
+
+        Scanner sc = new Scanner(System.in);
+
+        while (intentos > 0 && !haGanado) {
+            do {
+                System.out.println("introduce un número");
+                dato = sc.nextInt();
+
+            } while (dato < 20 || dato > 80);
+
+            if (dato < numero) {
+                System.out.println("el número es mayor");
+                intentos--;
+            } else if (dato > numero) {
+                System.out.println("el número es menor");
+                intentos--;
+            } else  {
+
+                haGanado = true;
+
+            }
+
+        }
+
+        if (haGanado) {
+            System.out.println("has acertado");
+        } else {
+            System.out.println("has perdido");
+        }
+    }
+    
+}
