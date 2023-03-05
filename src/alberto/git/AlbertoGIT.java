dcd446ccbaaef7441add1a5e405d169ed5ca3cdb
nebra <nebra@LAPTOP-5DSVCS25>
2023-03-05 23:57:56 +0100

    cambio de valor de variable a 40

--- src/alberto/git/AlbertoGIT.java
+++ src/alberto/git/AlbertoGIT.java
@@ -12,7 +12,7 @@ public class AlbertoGIT {
     public static void main(String[] args) {
         
          int dato;
-        int intentos = 20;
+        int intentos = 40;
         boolean haGanado = false;
 
         int numero = (int) ((Math.random() * 61) + 20);
